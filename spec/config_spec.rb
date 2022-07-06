21e1df1425ab788f3c7659240d6ebbb0b072d2e5
Murray Steele <muz@h-lame.com>
2022-07-06 09:56:00 +0100

    Upgrade to ruby 3.1
    This because Heroku told me the stack was out of date, so I upgraded it and the new stack doesn't have ruby 2.7.  Along the way we have to explicitly allow aliases when loading our YAML config as they're not enabled by default in the YAML parser in ruby 3.1.

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'config/meetcleo.yml' do
   it 'is valid YAML' do
-    config = YAML.load_file('config/meetcleo.yml')
+    config = YAML.load_file('config/meetcleo.yml', aliases: true)
 
     expect(config).to be_a(Hash)
   end
